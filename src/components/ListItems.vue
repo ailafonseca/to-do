4a783263ab5e781df208ae86bb9275a2692c457e
aila <ailafonseca16@gmail.com>
2023-10-19 23:17:12 -0300

    checkbox

--- src/components/ListItems.vue
+++ src/components/ListItems.vue
@@ -1,64 +1,51 @@
 <template>
-  <div class="row" id="center">
-    <div class="col-10">
-      <div class="input-group mb-3">
-        <input
-          v-model="newTask"
-          type="text"
-          class="form-control"
-          aria-label="Sizing example input"
-          aria-describedby="inputGroup-sizing-lg"
-          placeholder="Write your goal!"
-          id="input"
-          @keyup.enter="addNewTask"
-        />
-        <div class="input-group-append mx-3">
-          <button @click="addNewTask" :disabled="!newTask.length" class="btn btn-info" type="button">Add</button>
-          <button @click="deleteAllTasks" :disabled="!tasks.length" type="button" class="btn btn-danger ms-3">Delete List</button>
-        </div>
-      </div>
-    </div>
-    <div class="row" v-for="(task, i) in tasks" :key="i">
-      <div class="col" id="border">{{ task }}</div>
-      <div class="col-auto">
-        <button @click="editTask(i)" type="button" class="btn btn-warning"> 
-          <i class="bi bi-pencil-square"/>  
-        </button>
-        <button @click="deleteTask(i)" type="button" class="btn btn-danger ms-3">
-          <i class="bi bi-trash"></i>
-        </button>
-      </div>
+  <div class="input-group mb-3">
+    <input
+      v-model="newTask"
+      type="text"
+      class="form-control"
+      aria-label="Sizing example input"
+      aria-describedby="inputGroup-sizing-lg"
+      placeholder="Write your goal!"
+      id="input"
+      @keyup.enter="addNewTask"
+    />
+    <button
+      class="input-group-text"
+      @click="cancelNewTask"
+      :disabled="!newTask.length"
+      id="cancel-button"
+    >
+      <i class="bi bi-x-square-fill"></i>
+    </button>
+    <div class="input-group-append mx-3">
+      <button @click="addNewTask" :disabled="!newTask.length" class="btn btn-info" type="button">
+        Add
+      </button>
+      <button
+        @click="deleteAllTasks"
+        :disabled="!tasks.length"
+        type="button"
+        class="btn btn-danger ms-3"
+      >
+        Delete All Goals
+      </button>
     </div>
   </div>
+
+  <div class="d-flex m-2" v-for="(task, i) in tasks" :key="i">
+    <div class="form-check col">
+      <input class="form-check-input" type="checkbox" v-model="checkedTask[i]" />
+      <p :class="['border border-2 text-break', checkedTask[i] ? 'text-decoration-line-through' : '']">{{ task }}</p>
+    </div>
+    <button @click="editTask(i)" type="button" class="btn btn-warning">
+      <i class="bi bi-pencil-square" />
+    </button>
+    <button @click="deleteTask(i)" type="button" class="btn btn-danger ms-3">
+      <i class="bi bi-trash"></i>
+    </button>
+  </div>
 </template>
-
-<style>
-
-#center{
-  display: flex;
-  justify-content: center;
-}
-
-#input{
-  border: 2px solid rgb(156, 156, 156);
-  font-size: 20px;
-  align-items: center;
-  background-color: white;
-  text-wrap: balance;
-}
-
-#border {
-  border: 1px solid rgb(156, 156, 156);
-  padding: 10px;
-  margin: 0.04px;
-  margin-left: 10px;
-  background-color: white;
-  max-width: 100%;
-  inline-size: 300px;
-  overflow-wrap: break-word;
-}
-
-</style>
 
 <script lang="ts" setup>
 import { ref } from 'vue'
@@ -66,7 +53,7 @@ const newTask = ref('')
 const tasks = ref<string[]>([])
 let isEditing = ref(false)
 let selectedIndex = ref(-1)
-
+let checkedTask = ref([])
 
 const addNewTask = () => {
   //if (newTask.value.length < 1) return
@@ -78,7 +65,6 @@ const addNewTask = () => {
     tasks.value.push(newTask.value)
     newTask.value = ''
   }
-
 }
 
 const editTask = (i) => {
@@ -93,5 +79,9 @@ const deleteTask = (i) => {
 
 const deleteAllTasks = () => {
   tasks.value.splice(0)
+}
+
+const cancelNewTask = () => {
+  newTask.value = ''
 }
 </script>
